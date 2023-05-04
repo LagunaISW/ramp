98962a235440a46273ff1e12b34833b472220bec
Jhonatan Laguna <hola@jhonatanlaguna.com>
2023-05-04 12:32:40 -0700

    Extract extra data from properties

--- utils.js
+++ utils.js
@@ -22,6 +22,54 @@ function extractModels(schema) {
   return models;
 }
 
+function extractProperty(property) {
+  const [name, type, ...rest] = property.trim().split(/\s+/);
+  const result = {
+    name,
+    type,
+    optional: false,
+    relation: false,
+    defaultValue: null,
+    generated: false,
+  };
+  // If type contains a '?' or has a default value, it's an optional property
+  if (type.includes('?') || rest.some((word) => word.includes('@default'))) {
+    result.type = type.replace('?', '');
+    result.optional = true;
+  }
+  // If a element of 'rest' array contains '@relation', it's a relation property
+  const relationIndex = rest.findIndex((word) => word.includes('@relation'));
+  if (relationIndex !== -1) {
+    result.relation = true;
+    const relationString = rest[relationIndex + 1];
+    // Get the value from relationString ex: [PostToUser]
+    const value = relationString.match(/\[(.*)\]/)[1];
+    result.relationName = value;
+  }
+
+  // Get default value
+  const defaultIndex = rest.findIndex((word) => word.includes('@default'));
+  if (defaultIndex !== -1) {
+    const defaultString = rest[defaultIndex];
+    // Get the value from defaultString ex: @default("value") or @default(2) or @default(dbgenerated())
+    const value = defaultString.match(/@default\((.*)\)/)[1];
+
+    if (value.includes('(') && value.includes(')')) {
+      result.generated = true;
+    }
+
+    result.defaultValue = value;
+  }
+
+  if (name.includes('Id')) {
+    result.relationalId = true;
+  }
+
+  console.log({ property, result });
+
+  return result;
+}
+
 function extractProperties(modelName, schema) {
   const modelRegex = new RegExp(`model ${modelName} {([\\s\\S]*?)}`, 'g');
   const modelContent = modelRegex.exec(schema);
@@ -30,9 +78,9 @@ function extractProperties(modelName, schema) {
   const properties = modelContent[1]
     .trim()
     .split('\n')
+    .filter((line) => line.trim() !== '')
     .map((line) => {
-      const [property, type, ...rest] = line.trim().split(/\s+/);
-      return { name: property, type };
+      return extractProperty(line);
     })
     .filter(
       (property) =>
